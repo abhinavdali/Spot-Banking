fbf1af7cb614d33b019a81224dc795e1353e2f4a
Abhinav Raj. Dali <abhinav.dali@infodevelopers.local>
2022-11-15 11:52:10 +0545

    splash

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:spotbanking/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SpotBanking());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
